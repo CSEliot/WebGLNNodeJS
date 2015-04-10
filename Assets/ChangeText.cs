bf4833dc4834ef4953fa9c98cf5045b021f0aa3d
Eliot <Eliot@localhost>
2015-04-10 15:46:47 -0400

    Commit, missing Server code.

---
+++ Assets/ChangeText.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChangeText : MonoBehaviour 
+{
+    public Text ClientText;
+    public Text ServerText;
+    private string retrievedText;
+   
+	// Use this for initialization
+	/*void Start () 
+    {
+	    BeginDownloading = false;
+	}*/
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	}
+
+    public void POST()
+    {
+        string TextToSend = ClientText.text;
+        GameObject.Find("PretendServer").GetComponent<ServerPretend>().POST(TextToSend);
+    }
+
+    public void GET()
+    {
+        GameObject.Find("PretendServer").GetComponent<ServerPretend>().GET();
+    }
+
+    private void FARTS()
+    {
+        Debug.Log("Farts");
+    }
+}
---
+++ Assets/PostLocal.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PostLocal : MonoBehaviour {
+
+    public InputField ClientInputField;
+    public Text ClientText; 
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SendLocalText()
+    {
+        ClientText.text = ClientInputField.text;
+    }
+}
